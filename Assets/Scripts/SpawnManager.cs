4b26708be926012c682f673b41c8c33689985bfa
cgraf062491 <cgraf062491@gmail.com>
2021-05-12 14:52:58 -0400

    Christopher Graf - 05/11/21 - Wave System
    Successfully added a wave system. The number of enemies spawned is increased each wave. The enemies are spawned in a for loop, then once that loop is broken out of, there is a new WaitForSeconds() in the coroutine to wait an extended time for the next wave.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,7 @@ public class SpawnManager : MonoBehaviour
 	[SerializeField] private GameObject _enemyContainer;
 
 	private bool _stopSpawning;
+    private int _waveNum = 1;
     
 
     public void StartSpawning()
@@ -22,11 +23,17 @@ public class SpawnManager : MonoBehaviour
     	yield return new WaitForSeconds(3.0f);
     	while(_stopSpawning == false)
     	{
-    		float x_pos = Random.Range(-9.5f, 9.5f);
-    		GameObject newEnemy = Instantiate(_enemy, new Vector3(x_pos, 7.0f, 0), Quaternion.identity);
-    		newEnemy.transform.parent = _enemyContainer.transform;
-    		yield return new WaitForSeconds(2.0f);
-    	}
+            _waveNum += 1;
+            for(int i = 0; i < _waveNum; i++)
+            {
+                float x_pos = Random.Range(-9.5f, 9.5f);
+                GameObject newEnemy = Instantiate(_enemy, new Vector3(x_pos, 7.0f, 0), Quaternion.identity);
+                newEnemy.transform.parent = _enemyContainer.transform;
+                yield return new WaitForSeconds(2.0f);
+            }
+            yield return new WaitForSeconds(10.0f);
+        }
+    		
     }
 
     IEnumerator SpawnPowerup()
