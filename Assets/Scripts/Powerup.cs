b148956b9402b0ddea4f9a14c545a18c16893d03
cgraf062491 <cgraf062491@gmail.com>
2021-05-13 18:29:31 -0400

    Christopher Graf - 05/13/21 - Pickup Collect
    Powerups move towards the player while the 'C' key is held.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,9 +6,21 @@ public class Powerup : MonoBehaviour
 {
 	[SerializeField] private float _speed = 3.0f;
 	[SerializeField] private int _powerupID;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
 	
 	void Update()
 	{
+        if(Input.GetKey(KeyCode.C))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, _speed * Time.deltaTime);
+        }
+        
 		transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
 		if(transform.position.y < -6.0f)
